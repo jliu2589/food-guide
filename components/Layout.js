8ada1017596f43c588fd82c63b475dd09d3bf57e
Jay Liu <jliu2589@gmail.com>
2022-05-11 16:29:31 -0400

    create Layout.js and Layout.module.css

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/Layout.module.css";
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      <div className={styles.container}>{children}</div>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Food Guide | Finding the best Restaurants in the World!",
+  description:
+    "Searching for worthwild restaurants by aggregating the reviews from Michelin, Google Reviews, Tripadvisor, and top Influncers.",
+  keywords:
+    "Michelin Guide, Mark Weins, Trever James, Food Ranger, Bangkok, Fine Dinning",
+};
